27f90d53e8f9cec9b5bc2461d92335e99e427080
taehee <113824918+gmglgmgn@users.noreply.github.com>
2024-07-22 23:37:17 +0900

    [Bronze I] Title: 단어 공부, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[26] = {0};
+    
+    string str;
+    cin >> str;
+    
+    // 무조건 알파벳 대소문자로 이루어진 문자열만 들어온다고 가정.
+    for(int i = 0; i < str.length(); i++){
+        if(str[i] > 'Z'){ // 알파벳 대문자면 소문자로 만들기. ASCII 코드값 32 차이.
+            str[i] -= 32;        
+        }
+        arr[str[i] - 'A']++;    // 해당 알파벳 카운트 증가.
+    }
+    
+    int Max = 0;    // 가장 높은 알파벳 번호
+    bool notHighest = false;    // 빈도수가 가장 높은 알파벳이 하나 이상이면 true
+    for(int i = 1; i < 26; i++){
+        if(arr[Max] < arr[i]){ 
+            Max = i;
+            notHighest = false;
+        }
+        // else if((arr[Max] == arr[i]) && arr[Max] != 0){ 
+        // 이건 Max 교체되는 경우에 false로 플래그를 교체하면 논리 AND 대체 가능
+        else if(arr[Max] == arr[i]){
+            notHighest = true;
+        }
+    }
+    
+    if(notHighest == true){
+        cout << '?' << '\n';
+    }
+    else{
+        cout << (char)('A' + Max) << '\n';
+    }
+    
+    return 0;
+}
