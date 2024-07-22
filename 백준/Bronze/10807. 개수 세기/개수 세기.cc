c6e1960801ecb531aaaa9fd679de4424a3dec86c
taehee <113824918+gmglgmgn@users.noreply.github.com>
2024-07-22 20:46:33 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-//int arr[100];
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int arr[100];
+    int arr[201];
+    fill(arr, arr+201, 0);
     int N, V;
-    int CNT = 0;
     cin >> N;
+
     for (int i = 0; i < N; i++){
-        cin >> arr[i];
+        cin >> V;
+        arr[V+100]++;
     }
+    
     cin >> V;
-    for(int i = 0; i < N; i++){
-        if(arr[i] == V){
-            CNT++;
-        }
-    }
-    cout << CNT << '\n';
+    cout << arr[V+100] << '\n';
+    
+    return 0;
 }
