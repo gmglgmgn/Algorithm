b57dc40aaae92a286457f69fb3b560dacc0b6cb9
taehee <113824918+gmglgmgn@users.noreply.github.com>
2025-04-03 16:51:33 +0900

    [Bronze II] Title: 분해합, Time: 1704 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,41 @@
+'''
+문제
+어떤 자연수 N이 있을 때, 그 자연수 N의 분해합은 N과 N을 이루는 각 자리수의 합을 의미한다. 어떤 자연수 M의 분해합이 N인 경우, M을 N의 생성자라 한다. 예를 들어, 245의 분해합은 256(=245+2+4+5)이 된다. 따라서 245는 256의 생성자가 된다. 물론, 어떤 자연수의 경우에는 생성자가 없을 수도 있다. 반대로, 생성자가 여러 개인 자연수도 있을 수 있다.
+
+자연수 N이 주어졌을 때, N의 가장 작은 생성자를 구해내는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 자연수 N(1 ≤ N ≤ 1,000,000)이 주어진다.
+
+출력
+첫째 줄에 답을 출력한다. 생성자가 없는 경우에는 0을 출력한다.
+'''
+import sys
+input = sys.stdin.readline
+
+# 자연수 N 받기
+N = int(input().strip())
+
+# 생성자? N = M + M_0 + M_1 + ... + M_n
+# EX) 245 -> 2 + 4 + 5 + 245 = 256 -> 256이 N, 245가 생성자.
+# N의 자리수 부터 구하는 게 맞겠지?
+# 0 부터 올라가는게 맞겠지?
+constructor = 0
+for i in range(N+1):
+    sumOfdigits = 0
+    # 자리수 구하기 편하게 str로 변경.
+    strI = str(i)
+
+    # 각 자릿수의 합 구하기
+    for j in range(len(strI)):
+        sumOfdigits += int(strI[j])
+
+    # 분해합 구하기
+    decomposedSum = sumOfdigits + i
+
+    # 분해합이 N이면 생성자.
+    if decomposedSum == N:
+        constructor = i
+        break
+
+print(constructor)
