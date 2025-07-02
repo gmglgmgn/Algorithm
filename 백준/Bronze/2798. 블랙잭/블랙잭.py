159a19ceacaf8c40cbac318227414815c15979f6
taehee <113824918+gmglgmgn@users.noreply.github.com>
2025-07-02 14:56:17 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,15 +1,15 @@
 import sys
 input = sys.stdin.readline
-from itertools import combinations
 
 N, M = map(int, input().split())
-lst = list(map(int, input().split()))
+cards = list(map(int, input().split()))
 res = 0
-s = 0
 
-for i, j, k in combinations(range(N), 3):
-    s = lst[i] + lst[j] + lst[k]
-    if M >= s > res:
-        res = s
-        
+for i in range(0, N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            s = cards[i] + cards[j] + cards[k]
+            if M >= s > res:
+                res = s
+
 print(res)
